8cd8705647ffd7c07f633e2f31bb8c7fe518627f
ismaildedec <ismaildedec@gmail.com>
2021-08-09 21:30:28 +0300

    Writeline-Readlıne çalışması eklendi.

---
+++ Writeline-Readlıne/Writeline-Readlıne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Writeline_Readlıne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name :"); // WriteLine'da imleç direkt alt satıra geçer.
+            string name = Console.ReadLine(); //string te ReadLine komutu kullan !
+            Console.WriteLine("WELCOME "+name +" !!! \n");
+            Console.WriteLine("---------------------------------------");
+            Console.Write("Yasınız :");
+            string age = Console.ReadLine();
+            Console.WriteLine(age+" Yasındasın");
+            Console.WriteLine("---------------------------------------");
+            Console.Write("What is your name :"); // WriteLine'da imleç direkt alt satıra geçer.
+            int acicode = Console.Read(); //int te Read komutu kullan !
+            Console.WriteLine(acicode);
+            Console.ReadKey();
+        }
+    }
+}
