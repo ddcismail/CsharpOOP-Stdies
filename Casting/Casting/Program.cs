adf4f316eb2979b749e53a6f0db578437b93888f
ismaildedec <ismaildedec@gmail.com>
2021-08-09 23:22:37 +0300

    Casting(Tür Dönüşümü) çalışması eklendi.

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 12;
+            short y = 10;
+            int z = 8;
+            long t = 5;
+            float k = 13;
+            // k=t;  (doğru) küçüğü büyüğe atıyoruz.
+            // x=y ;(yanlış)
+            // y=x ;(doğru)
+            // string f;  Hata alırız çünkü değişken tipleri farklıdır Explicit Casting olarak yapmamız gerek 
+
+            double i = 24;
+            int h;
+            h = (int)i; //Explicit 
+
+            double f = 24;
+            int g;
+            g = Convert.ToInt32(f);//Convert
+            string g = f.ToString(); // ToString her değişken türüne çevriliri.
+        }
+    }
+}
