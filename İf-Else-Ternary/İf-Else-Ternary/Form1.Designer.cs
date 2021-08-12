df6b579c4a6c45ce9d34c878ceeff14bfdb5067a
ismaildedec <ismaildedec@gmail.com>
2021-08-12 21:04:56 +0300

    If-Else-Ternary çalışması eklendi.

---
+++ İf-Else-Ternary/İf-Else-Ternary/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace İf_Else_Ternary
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnTurnary = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(46, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnTurnary
+            // 
+            this.btnTurnary.Location = new System.Drawing.Point(567, 160);
+            this.btnTurnary.Name = "btnTurnary";
+            this.btnTurnary.Size = new System.Drawing.Size(121, 38);
+            this.btnTurnary.TabIndex = 4;
+            this.btnTurnary.Text = "Ternary";
+            this.btnTurnary.UseVisualStyleBackColor = true;
+            this.btnTurnary.Click += new System.EventHandler(this.btnTurnary_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(46, 168);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(184, 30);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnTurnary);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnTurnary;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ İf-Else-Ternary/İf-Else-Ternary/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İf_Else_Ternary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Aracı Almak İstiyor Musun ?","İf control",MessageBoxButtons.YesNoCancel,MessageBoxIcon.Warning);
+            if (result==DialogResult.Yes)
+            {
+                textBox1.Text="Evet almak istiyorum";
+            }
+            else if (result==DialogResult.No)
+            {
+                textBox1.Text = "Hayır almak istemiyorum";
+            }
+            else 
+            {
+                textBox1.Text = "Geri çıktınız";
+            }
+        }
+
+        private void btnTurnary_Click(object sender, EventArgs e)
+        {
+            int a = 40, b = 30;
+            string turnaryresult;
+            turnaryresult = a < b ? a.ToString(" doğru a b'den küçük"): b.ToString("yanlış b a dan küçük") ; //soru işareti öncesi sorgu blogudur burası true dönerse birinci koşul, false ise ikinci koşul döner
+            textBox3.Text = turnaryresult;        
+        }
+    }
+}
