6a0dddd140db0acb2c68b07973368dd82bf9c3f4
ismaildedec <ismaildedec@gmail.com>
2021-08-13 12:46:00 +0300

    SwitchCase çalışması eklendi.

---
+++ SwitchCase/Switch-Case/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_Case
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string messagtex = "";
+            switch (Convert.ToInt32(textBox1.Text))
+            {
+                case 2:
+                    messagtex = "x=2";
+                    break;
+                case 3:
+                    messagtex = "x=3";
+                    break;
+                case 4:
+                    messagtex = "x=4";
+                    switch (Convert.ToInt32(textBox2.Text))
+                    {
+                        case 2:
+                            messagtex += "y=2";
+                            break;
+                        case 3:
+                            messagtex += "y=3";
+                            break;
+                        case 4:
+                            messagtex += "y=4";
+                            break;
+                        case 5:
+                            messagtex += "y=5";
+                            break;
+                        default:
+                            messagtex += "yanlış sayı yazdınız";
+                            break;
+                    }
+                    break;
+                case 5:
+                    messagtex = "x=5";
+                    break;
+                default:
+                    messagtex = "yanlış sayı yazdınız";
+                    break;
+            }
+            MessageBox.Show(messagtex);
+        }
+    }
+}
---
+++ SwitchCase/Switch-Case/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Switch-Case")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Switch-Case")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("aa8fa91c-b4a6-46ed-8df4-31e91168db5d")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
